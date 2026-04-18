f56c8f6e4c903b70bcd0a8ec876a6eb458d538ed
kenshi16216 <kenshi16216@gmail.com>
2026-04-18 17:24:48 +0900

    fix: add base path and update node version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ここが重要！「/リポジトリ名/」を正確に指定します
+  base: '/study-log/', 
 })
